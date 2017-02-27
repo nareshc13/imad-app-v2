111f4439ba11e402c6508085d3b1719ae768a47a
nareshc13 <naresh1995ch@gmail.com>
2017-02-27 13:22:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send(createtemplate(articleTwo));
+  res.send(createTemplate(articleTwo));
 });
 
 
