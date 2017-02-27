87094a0798cb5066cde686136f72401ff3302599
nareshc13 <naresh1995ch@gmail.com>
2017-02-27 16:01:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHTML='new value';
+var  img=document.getElementById('madi');
+
+img.onclick=function(){
+    var interval=setInterval(moveRight,10);
+img.style.marginLeft='100px';
+};
+marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+"px";
+}
