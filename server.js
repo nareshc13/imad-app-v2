0262ccee29a51103bd89212b5b8c5d937686c2f1
nareshc13 <naresh1995ch@gmail.com>
2017-03-08 22:58:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/article-two', function (req, res) {
 app.get('/articles/:articleName', function (req, res) {
 //  var articleName=req.params.articleName;
   //res.send(createTemplate(articleData));
-  pool.query("SELECT * FROM article WHERE title=$1" [req.params.articleName], function(err,result){
+  pool.query("SELECT * FROM article WHERE title=$1" ,[req.params.articleName], function(err,result){
      if(err){
          res.send(500).send(err.toString());
      } else
