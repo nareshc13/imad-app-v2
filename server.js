1f4f297617d45c4a7782254575af29af6cbaad75
nareshc13 <naresh1995ch@gmail.com>
2017-03-08 19:26:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
 
 var app = express();
 app.use(morgan('combined'));
 
+var config={
+    user:'nareshc13',
+    database:'nareshc13',
+    host:'db.imad.hasura-app.io',
+    password:process.env.DB_PASSWORD,
+    port:'5432',
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+   pool.query('SELECT * FROM test', function(err,reult){
+      if(err){
+          res.status(500).send(err.toString());
+      } 
+      else{
+          res.send(JSON.stringify(result));
+      }
+   });
+   
 });
 
 app.get('/', function (req, res) {
