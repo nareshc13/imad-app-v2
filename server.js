9cc1ea82500dab72e5773505ba797cb495ccf25d
nareshc13 <naresh1995ch@gmail.com>
2017-03-10 18:47:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/test-db', function(req, res){
 });
 
 function hash(input,salt){
-    var hashed=crypto.pbkdf2Sync('input','salt',10000,512,'sha12');
+    var hashed=crypto.pbkdf2Sync('input','salt',10000,512,'sha512');
     return hashed.toString('hex');
 }
 
