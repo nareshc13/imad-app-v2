d722f16e427b18a3115d79f1151a2c7c8cfa398e
nareshc13 <naresh1995ch@gmail.com>
2017-03-15 12:41:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post('/login', function(req, res){
     var password=req.body.password;
    
     var dbString= hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString], function(err, result){
+    pool.query('SELECT * FROM "user" (username,password) VALUES ($1,$2)',[username,dbString], function(err, result){
         if(err){
             res.status(500).send(err.toString());
             }
