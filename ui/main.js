1d4e590433bb682719e2569aff016b6d6cd3e252
nareshc13 <naresh1995ch@gmail.com>
2017-03-15 17:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,30 @@
-console.log('Loaded!');
+
+var submit=document.getElementById('submit');
+submit.onclick= function() {
+  var request=new XMLHttpRequest();
+  request.onreadystatechange= function() {
+    if(request.readyState===XMLHttpRequest.DONE) {
+        if(request.status===200) {
+            alert('successfully logged in');
+        } else if(request.status===403) {
+    alert('username/password is invalid');
+        }  else if(request.status===500){
+            alert('something went wrong on server')
+        }
+        }  
+  };
+};
+
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+
+request.open('POST', 'http://nareshc13.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
+
+/*console.log('Loaded!');
 //var element=document.getElementById('main-text');
 //element.innerHTML='new value';
 var  img=document.getElementById('madi');
@@ -11,4 +37,4 @@ marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+"px";
-}
+} */
